4192ada60c937c0981dd86d29a141f20bbe459b0
litmiz <litmiz07@gmail.com>
2020-05-15 17:14:55 +0300

    Added ex2 HTML and JS

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function ex1(userName, password) {
         document.getElementById('logMsg').textContent = 'שם המשתמש או הסיסמה אינם נכונים';       
     }
 }
+
+document.getElementById('namesBtn').addEventListener('click', function(){
+    ex2();
+})
+
+function ex2() {
+    var names = Array('אריאל', 'לילך', 'ברק', 'נדב', 'כפיר');
+    document.getElementById('names').textContent = '';
+    for (i = 0; i < names.length; i++) {
+        document.getElementById('names').textContent += names[i] +', ';
+        console.log(names[i]);
+    }
+}
